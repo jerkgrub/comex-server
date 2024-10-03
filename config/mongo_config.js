f7a36f1a0e4a55fe1143a8c178d7edf7f0fa18d7
Jerick Nucasa <nucasa.jerick@gmail.com>
2024-10-03 17:27:09 +0800

    for vercel

--- config/mongo_config.js
+++ config/mongo_config.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const MONGODB_URI =
   process.env.MONGODB_URI ||
   "mongodb+srv://nucasajerick:5sAb73lTrLX0c6Fy@cluster0.khsbida.mongodb.net/comexconnect";
- //  "mongodb://127.0.0.1:27017/comexconnect"
+ //  "mongodb://127.0.0.1:27017/comexconnect" KEEP INCASE WANT TO TEST LOCALLLY
 
 mongoose
   .connect(MONGODB_URI)
--- server.js
+++ server.js
@@ -1,11 +1,30 @@
+// const express = require("express");
+// const app = express();
+// const port = 8000;
+// require("./config/mongo_config");
+// const cors = require("cors");
+// app.use(express.json(), express.urlencoded({ extended: true }), cors());
+
+// //introduce your routes to the server
+// const UserRoute = require("./routes/user_routes");
+// const ActivityRoute = require("./routes/activity_routes");
+// const CreditRoute = require("./routes/credit_routes");
+
+// app.use(UserRoute);
+// app.use(ActivityRoute);
+// app.use(CreditRoute);
+
+// app.listen(port, () => console.log("Chigga, the server is all fired up on port 8000"));
+
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;  // Use the port assigned by Vercel or default to 8000 for local dev
 require("./config/mongo_config");
 const cors = require("cors");
+
 app.use(express.json(), express.urlencoded({ extended: true }), cors());
 
-//introduce your routes to the server
+// Introduce your routes to the server
 const UserRoute = require("./routes/user_routes");
 const ActivityRoute = require("./routes/activity_routes");
 const CreditRoute = require("./routes/credit_routes");
@@ -14,4 +33,4 @@ app.use(UserRoute);
 app.use(ActivityRoute);
 app.use(CreditRoute);
 
-app.listen(port, () => console.log("Chigga, the server is all fired up on port 8000"));
+app.listen(port, () => console.log(`Chigga, the server is all fired up on port ${port}`));
