74180a65da864a3cc35e67860de0392d507ea337
JC <nucasa.jerick@gmail.com>
2024-10-15 17:03:59 +0800

    removed required (lazy testing)

--- models/credit_model.js
+++ models/credit_model.js
@@ -5,63 +5,40 @@ const CreditSchema = new mongoose.Schema(
     activityId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Activity",
-      required: function () {
-        return this.isRegisteredEvent === true;
-      },
     },
     isRegisteredEvent: {
       type: Boolean,
-      required: true,
     },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
     type: {
-      type: String,
-      required: true, // Type is required for both registered and non-registered events
+      type: String, 
     },
     title: {
       type: String,
-      required: function () {
-        return this.isRegisteredEvent === false;
-      },
     },
     isVoluntary: {
       type: Boolean,
-      required: function () {
-        return this.isRegisteredEvent === false;
-      },
     },
     beneficiaries: {
       type: String,
-      required: function () {
-        return this.isRegisteredEvent === false;
-      },
     },
     startDate: {
       type: Date,
-      required: function () {
-        return this.isRegisteredEvent === false;
-      },
     },
     endDate: {
       type: Date,
-      required: function () {
-        return this.isRegisteredEvent === false;
-      },
     },
     totalHoursRendered: {
       type: Number,
-      required: true,
     },
     supportingDocuments: {
       type: String, // Store URL or path to the uploaded file
     },
     facultyReflection: {
       type: String,
-      required: true,
     },
   },
   { timestamps: true }
