783b97df26d0bca493fd6f1ab0a6ebbbb6aa7d75
jerkgrub <nucasa.jerick@gmail.com>
2025-03-30 15:46:22 +0800

    make better routes

--- routes/notification_routes.js
+++ routes/notification_routes.js
@@ -1,33 +1,17 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const notificationController = require("../controllers/notification_controller");
+const notificationController = require('../controllers/notification_controller');
 
-// # add routes for admin side notifications here
+// Get all notifications for a specific user
+router.get('/:userId', notificationController.getNotificationsForUser);
 
-// Get all notifications for a specific user (admin side)
-router.get(
-  "/admin/:userId",
-  notificationController.getNotificationsForUser
-);
+// Get all unread notifications for a specific user
+router.get('/:userId/unread', notificationController.getUnreadNotificationsForUser);
 
-// Get all unread notifications for a specific user (admin side)
-router.get(
-  "/admin/:userId/unread",
-  notificationController.getUnreadNotificationsForUser
-);
+// Get all unread notifications total count for a specific user
+router.get('/:userId/unread/count', notificationController.getUnreadNotificationsCountForUser);
 
-// Get all unread notifications total count for a specific user (admin side)
-router.get(
-  "/admin/:userId/unread/count",
-  notificationController.getUnreadNotificationsCountForUser
-);
-
-// Turn notification as read (admin side)
-router.put(
-  "/admin/:notificationId",
-  notificationController.turnNotificationAsRead
-);
-
-// #add routes for client side notifications here
+// Turn notification as read
+router.put('/:notificationId', notificationController.turnNotificationAsRead);
 
 module.exports = router;
