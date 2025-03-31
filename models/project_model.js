ad48117a0cfe21a96b683421a946c96199df0f65
jerkgrub <nucasa.jerick@gmail.com>
2025-04-01 00:04:48 +0800

    ADD: important datatypes for institutional activity

--- models/project_model.js
+++ models/project_model.js
@@ -147,9 +147,13 @@ const ProjectSchema = new mongoose.Schema(
     // ----------------------------------------------------------------------------------------------------------------------------
     // these are now for projects that have engagementType "Institutional"
     // REMOVED: registrationStart, registrationEnd, it will just be common-sense that once the project has been created, it will be available for registration until startDate
+    registrationStart: String,
+    registrationEnd: String,
+    registrationStartTime: String,
+    registrationEndTime: String,
     thumbnail: String,
     // wait muna natin yung wireframe ni ban
-    
+
   },
 
   { timestamps: true }
